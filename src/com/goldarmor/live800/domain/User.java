41e21365ce43b18452ba0ec8276e9e6366338df7
tongxinershi <270240692@qq.com>
2018-03-05 21:14:51 +0800

    company test commit

--- src/com/goldarmor/live800/domain/User.java
+++ src/com/goldarmor/live800/domain/User.java
@@ -2,5 +2,14 @@ package com.goldarmor.live800.domain;
 
 public class User {
 	private String username;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
